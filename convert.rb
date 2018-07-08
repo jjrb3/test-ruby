790782f8dd18de34ebd1659a536e37ef93ea3611
= <jjrb6@hotmail.com>
2018-07-08 15:11:57 -0500

    [feat]: Convert values.

---
+++ convert.rb
@@ -0,0 +1,7 @@
+interger = '1'
+float    = '1.2'
+string  = 1
+
+puts interger.to_i # Convert to interger
+puts float.to_f    # Convert to float
+puts string.to_s   # Convert to string
--- hello-word.rb
+++ hello-word.rb
@@ -1,7 +1,15 @@
+# Entras al interprete con irb
 # Este es un comentario
-puts "Hello word!" # Sirve para una sola linea
+puts "Hello word! " + 2.to_s # Convertir numerico en string.
+
+puts '2'.to_i # Convertir a numerico
+
+name = "Jeremy"
+
+puts "Hello #{name}"
 
 =begin
 Comentario para varias lineas.
 Es raro, lo sé.
 =end
+
